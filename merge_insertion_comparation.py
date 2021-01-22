87a69e3d359aabc51d6a1ddbcf0ae47d3f50b997
ileosebastian <ileosebastian@gmail.com>
2021-01-22 03:50:23 -0500

    Ingresé el algoritmo de insercion simplemente

--- merge_insertion_comparation.py
+++ merge_insertion_comparation.py
@@ -1,11 +1,2 @@
 # modules that to need it
-
-
-# read the cvs or a simply list
-
-# If the input is a cvs file:
-
-# If the input is a simple list as a set values:
-
-
-# execute insertion and merge algorithm (parallel programming)
+import random
---
+++ separate-algorithms/insertion_sort.py
@@ -0,0 +1,36 @@
+import re
+
+print("ALGORITMO DE INSERCION.")
+print("\tIngrese numeros y luego presione ENTER", 
+        "\n\tSi quiere terminar su lista, presione t",
+        "\n\tSi quiere salir del programa, presione q")
+
+numbers = []
+
+flag = True
+i = 1
+while(flag or not numbers):
+    var = input(f'[{i}]:\t')
+    if not re.match('/[1-9]+/g',var) or var == 'q' or var == 't':
+        if var is 't': # if terminated of type numbers
+            flag = False
+        elif var is 'q': # if wish to quit the program
+            flag = False
+        else:
+            numbers.append(int(var))
+    i += 1
+
+if var is 't':
+    # insertion sort
+    for j in range(1,len(numbers)):
+
+        key = numbers[j]
+
+        i = j - 1
+        while i >= 0 and numbers[i] > key:
+            numbers[i+1] = numbers[i]
+            i = i - 1
+        numbers[i+1] = key
+
+print("List in order: ",
+      str(numbers))

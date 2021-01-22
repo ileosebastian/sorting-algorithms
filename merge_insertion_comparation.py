7f5cb4334f2d658e8180795dc703fe75e0fe7c49
ileosebastian <ileosebastian@gmail.com>
2021-01-22 01:52:46 -0500

    Primer commit, para verificar que me acuerdo

---
+++ merge_insertion_comparation.py
@@ -0,0 +1,11 @@
+# modules that to need it
+
+
+# read the cvs or a simply list
+
+# If the input is a cvs file:
+
+# If the input is a simple list as a set values:
+
+
+# execute insertion and merge algorithm (parallel programming)
